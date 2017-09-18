96114380d6512cdb6e06fe7173c248d005fe6e5f
Nelson Yim <ncl.yim@gmail.com>
2017-09-18 00:31:06 -0700

    Removed argument for rolling

--- d20.py
+++ d20.py
@@ -4,16 +4,14 @@ import argparse
 from random import randint
 
 parser = argparse.ArgumentParser()
-parser.add_argument('roll')
 parser.add_argument('-th', '--threshold', default=False, dest='threshold')
 parser.add_argument('-t', '--task', default=None, dest='task')
 parser.add_argument('-s', '--stat', default=None, dest='stat')
 args = parser.parse_args()
 
 
-if args.roll == 'd20':
-	num = randint(1, 20)
-	print 'roll: {}'.format(num)
+roll = randint(1, 20)
+print 'roll: {}'.format(roll)
 
 if args.threshold:
 	num = randint(1, 20)
